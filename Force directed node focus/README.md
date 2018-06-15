bf71216ce08027853937da6550f5d3465312698b
Dhanushanth Pushparajah <dhanushanthp@Dhanushanths-MacBook-Pro.local>
2018-06-15 19:00:02 +0800

    initial commit

---
+++ Force directed node focus/README.md
@@ -0,0 +1,2 @@
+#  Force directed node focus
+The data has been distributed in `data.js` file. The links can be connected based on `node` values appears order. For example the first values in `node` can be considered as 0th position. So to link this with different using `link` then value will be 0.
---
+++ Force directed node focus/data.js
@@ -0,0 +1,77 @@
+window['mcpherTreeData'] = 
+{
+  "d3": {
+    "options": {
+      "radius": 2.5,
+      "fontSize": 9,
+      "labelFontSize": 9,
+      "gravity": 0.1,
+      "height": 800,
+      "nodeFocusColor": "black",
+      "nodeFocusRadius": 25,
+      "nodeFocus": true,
+      "linkDistance": 150,
+      "charge": -220,
+      "nodeResize": "count",
+      "nodeLabel": "label",
+      "linkName": "tag"
+    },
+    "data": {
+      "nodes": [
+        {
+          "name": "Home",
+          "count": 100,
+          "group": "vbaExcel",
+          "linkCount": 100,
+          "label": "Services",
+          "userCount": true,
+          "url": "https://sites.google.com/a/mcpher.com/share/Home",
+          "shortName": "Home"
+        },
+        {
+          "name": "vbaExcel",
+          "count": 1358,
+          "group": "vbaExcel",
+          "linkCount": 235,
+          "label": "Services",
+          "userCount": true,
+          "url": "https://sites.google.com/a/mcpher.com/share/Home",
+          "shortName": "vbaExcel"
+        },
+        {
+          "name": "excelquirks",
+          "count": 84,
+          "group": "d3",
+          "linkCount": 16,
+          "label": "Excel Liberation",
+          "userCount": true,
+          "url": "https://sites.google.com/a/mcpher.com/share/Home/excelquirks",
+          "shortName": "excelquirks"
+        }
+      ],
+      "links": [
+        {
+          "source": 0,
+          "target": 1,
+          "depth": 90,
+          "count": 1,
+          "linkName": "https://sites.google.com/a/mcpher.com/share/Home"
+        },
+        {
+          "source": 1,
+          "target": 2,
+          "depth": 90,
+          "count": 100,
+          "linkName": "https://sites.google.com/a/mcpher.com/share/Home/excelquirks"
+        },
+        {
+          "source": 2,
+          "target": 0,
+          "depth": 9,
+          "count": 1,
+          "linkName": "https://sites.google.com/a/mcpher.com/share/Home/excelquirks"
+        }
+      ]
+    }
+  }
+}
